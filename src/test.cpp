7cb5b60a716cfa1058ee21c7659ed8331eddeddf
discodyer <cody23333@gmail.com>
2023-07-07 20:40:27 +0800

    尝试直接跳过检查

--- src/test.cpp
+++ src/test.cpp
@@ -136,28 +136,30 @@ int main(int argc, char **argv)
             break;
 
         case takeoff:
-
-            if (takeoff_client.call(takeoff_cmd) &&
-                (ros::Time::now() - last_request > ros::Duration(1.0)))
-            {
-                ROS_INFO("Vehicle Takeoff!");
-                drone_state = land;
-            }
-            else
-            {
-                ROS_INFO("Vehicle Takeoff Failed!");
-            }
-            if (!current_state.armed)
-            {
-                drone_state = arm;
-            }
+            ROS_INFO("Vehicle Takeoff: %d", takeoff_client.call(takeoff_cmd));
+            drone_state = land;
             last_request = ros::Time::now();
+            // if (takeoff_client.call(takeoff_cmd) &&
+            //     (ros::Time::now() - last_request > ros::Duration(1.0)))
+            // {
+            //     ROS_INFO("Vehicle Takeoff!");
+            //     drone_state = land;
+            // }
+            // else
+            // {
+            //     ROS_INFO("Vehicle Takeoff Failed!");
+            // }
+            // if (!current_state.armed)
+            // {
+            //     drone_state = arm;
+            // }
+            // last_request = ros::Time::now();
 
             break;
 
         case land:
             if (!current_state.armed &&
-                (ros::Time::now() - last_request > ros::Duration(5.0)))
+                (ros::Time::now() - last_request > ros::Duration(10.0)))
             {
                 mavros_msgs::CommandTOL land_cmd;
                 if (land_client.call(land_cmd) &&
