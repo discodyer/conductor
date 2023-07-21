6a409e59c60bbbafcb7f56243f9926c3d44e9886
discodyer <cody23333@gmail.com>
2023-07-21 12:05:16 +0800

    修改了pid控制器

--- include/conductor/pid_controller.h
+++ include/conductor/pid_controller.h
@@ -1,27 +1,42 @@
 #ifndef PID_CONTROLLER_H
 #define PID_CONTROLLER_H
 
-#include <ros/ros.h>
+#include <ros/time.h>
+#include <ros/duration.h>
 
 class PIDController {
 public:
-    PIDController(double kp, double ki, double kd, double outputMin, double outputMax);
-    double compute(double setpoint, double input, double dt);
+    PIDController(double kp, double ki, double kd,double windup_guard, double output_bound, double sample_time = 0.1);
+    double calculateOutput(double feedback_value);
+    double getBoundedOutput(double feedback_value);
+    void clear();
+
+    void setKp(double proportional_gain);
+    void setKi(double integral_gain);
+    void setKd(double derivative_gain);
+    void setWindup(double windup);
+    void setSampleTime(double sample_time);
+    void setOutputBound(double output_bound);
 
 private:
-    double m_kp;
-    double m_ki;
-    double m_kd;
-    double m_outputMin;
-    double m_outputMax;
-    double m_error;
-    double m_lastError;
-    double m_integral;
-    double m_derivative;
+    double kp_;
+    double ki_;
+    double kd_;
+    double output_bound_;
+    double error_;
+    double last_error_;
+    double integral_;
+    double derivative_;
+    double windup_guard_;
+    double setpoint_;
+    double p_term_;
+    double i_term_;
+    double d_term_;
+    double last_control_output_;
 
-    double m_sample_time;
-    ros::Time m_last_time;
-    ros::Time m_current_time;
+    double sample_time_;
+    ros::Time last_time_;
+    ros::Time current_time_;
 };
 
 #endif // PID_CONTROLLER_H
--- src/lib/pid.cc
+++ src/lib/pid.cc
@@ -1,37 +1,94 @@
 #include "conductor/pid_controller.h"
+#include "conductor/bound.h"
 
-PIDController::PIDController(double kp, double ki, double kd, double outputMin, double outputMax)
-    : m_kp(kp), m_ki(ki), m_kd(kd), m_outputMin(outputMin), m_outputMax(outputMax),
-      m_error(0.0), m_lastError(0.0), m_integral(0.0), m_derivative(0.0)
+PIDController::PIDController(double kp, double ki, double kd, double windup_guard, double output_bound, double sample_time)
+    : kp_(kp), ki_(ki), kd_(kd), windup_guard_(windup_guard), output_bound_(output_bound), sample_time_(sample_time),
+      error_(0.0), last_error_(0.0), integral_(0.0), derivative_(0.0), last_control_output_(0.0)
 {
+    last_time_ = ros::Time::now();
+    current_time_ = ros::Time::now();
+    clear();
 }
 
-double PIDController::compute(double setpoint, double input, double dt)
+void PIDController::clear()
+{
+    last_error_ = 0.0;
+    setpoint_ = 0.0;
+    p_term_ = 0.0;
+    i_term_ = 0.0;
+    d_term_ = 0.0;
+    last_control_output_ = 0.0;
+}
+
+double PIDController::calculateOutput(double feedback_value)
 {
     // 计算误差
-    m_error = setpoint - input;
+    double error = setpoint_ - feedback_value;
 
-    // 计算积分项
-    m_integral += m_error * dt;
+    // 计算距离上次更新经过的时间
+    double delta_time = (ros::Time::now() - last_time_).toSec();
 
-    // 计算微分项
-    m_derivative = (m_error - m_lastError) / dt;
+    // 计算误差的变化率
+    double delta_error = error - last_error_;
 
-    // 计算输出
-    double output = m_kp * m_error + m_ki * m_integral + m_kd * m_derivative;
-
-    // 限制输出范围
-    if (output < m_outputMin)
+    // 检查是否满足采样时间条件
+    if (delta_time >= sample_time_)
     {
-        output = m_outputMin;
-    }
-    else if (output > m_outputMax)
-    {
-        output = m_outputMax;
-    }
+        // 计算比例项
+        p_term_ = kp_ * error;
 
-    // 保存上一次的误差
-    m_lastError = m_error;
+        // 计算积分项并防止积分饱和
+        i_term_ += error * delta_time;
+        i_term_ = Bound<double>(i_term_, windup_guard_);
 
-    return output;
+        // 计算微分项
+        d_term_ = 0.0;
+        if (delta_time > 0)
+        {
+            d_term_ = delta_error / delta_time;
+        }
+
+        // 更新上次更新时间和上次误差
+        last_time_ = ros::Time::now();
+        last_error_ = error;
+
+        // 计算PID控制器输出并保存为上次控制输出
+        last_control_output_ = p_term_ + (ki_ * i_term_) + (kd_ * d_term_);
+    }
+    return last_control_output_;
+}
+
+double PIDController::getBoundedOutput(double feedback_value)
+{
+    return Bound<double>(calculateOutput(feedback_value), output_bound_);
+}
+
+void PIDController::setKp(double proportional_gain)
+{
+    kp_ = proportional_gain;
+}
+
+void PIDController::setKi(double integral_gain)
+{
+    ki_ = integral_gain;
+}
+
+void PIDController::setKd(double derivative_gain)
+{
+    kd_ = derivative_gain;
+}
+
+void PIDController::setWindup(double windup)
+{
+    windup_guard_ = windup;
+}
+
+void PIDController::setSampleTime(double sample_time)
+{
+    sample_time_ = sample_time;
+}
+
+void PIDController::setOutputBound(double output_bound)
+{
+    output_bound_ = output_bound;
 }
